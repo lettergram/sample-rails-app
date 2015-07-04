75e547c73bf5b6620b83ab7428cbf62fb87f9a2f
Lettergram <Lettergramm@gmail.com>
2015-07-04 16:20:25 -0700

    fixed sendgrid with proper host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'frozen-forest-9497.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
