9d0f1892fc6542560737fb0fd95a7de75c296019
Lettergram <Lettergramm@gmail.com>
2015-07-04 15:52:39 -0700

    updated spelling for :create_activation_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,6 +77,7 @@ class User < ActiveRecord::Base
       self.email = email.downcase
     end
 
+
     # Creates and assigns the activation token and digest.
     def create_activation_digest
       self.activation_token  = User.new_token
